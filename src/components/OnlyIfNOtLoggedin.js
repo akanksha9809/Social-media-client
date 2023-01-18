bb31f63c6fa047cf5abdab549d0311552b52caf7
Akanksha <akanksha.bodhane@gmail.com>
2023-01-18 17:39:15 +0530

    server url added

--- src/components/OnlyIfNOtLoggedin.js
+++ src/components/OnlyIfNOtLoggedin.js
@@ -1,12 +1,10 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom';
-import { getItem,KEY_ACCESS_TOKEN } from '../utils/localStorageManager'
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { getItem, KEY_ACCESS_TOKEN } from "../utils/localStorageManager";
 
 function OnlyIfNOtLoggedin() {
-    const user = getItem(KEY_ACCESS_TOKEN)
-  return (
-    user ? <Navigate to="/"/> : <Outlet/>
-  )
+  const user = getItem(KEY_ACCESS_TOKEN);
+  return user ? <Navigate to="/" /> : <Outlet />;
 }
 
-export default OnlyIfNOtLoggedin
+export default OnlyIfNOtLoggedin;
